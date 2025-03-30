a792011b28e999f63497f9f151343b0184a94bcd
Leinah <gtpndn@gmail.com>
2025-03-30 12:53:30 +0900

    Custom Verify Csrf Token

--- config/auth.php
+++ config/auth.php
@@ -40,6 +40,11 @@ return [
             'driver' => 'session',
             'provider' => 'users',
         ],
+        /* This is for Laravel Passport to enable microservices */
+        'api' => [
+            'driver' => 'passport',
+            'provider' => 'users',
+        ],
     ],
 
     /*
@@ -111,5 +116,8 @@ return [
     */
 
     'password_timeout' => env('AUTH_PASSWORD_TIMEOUT', 10800),
+
+    'password_client_id' => env('PASSWORD_CLIENT_ID'),
+    'password_client_secret' => env('PASSWORD_CLIENT_ID'),
 
 ];
